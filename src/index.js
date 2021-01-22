19eebcb2783d3664cf4e78d47302789b04e0b1e1
raghdatharwat97 <raghdatharwa97@gmail.com>
2021-01-22 19:43:32 +0200

    --

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
 const express = require ('express')
 
 const New = require('./models/new')
+const Reporter = require('./models/reporter')
 
 const newRouter = require('./routers/new-router')
+const reporterRouter = require('./routers/reporter-router')
 require('./db/mongoose')
 
 
@@ -12,10 +14,21 @@ const app = express()
 app.use(express.json())
 
 app.use(newRouter)
-
+app.use(reporterRouter)
 const port = 3000
 
 
+const jwt = require('jsonwebtoken')
+
+// const myToken = async()=>{
+//     const token = jwt.sign({_id:'123'},'node-course',{expiresIn:'7 days'})
+//     //console.log(token)
+//     const tokenVerify = jwt.verify(token,'node-course')
+//     //console.log(tokenVerify)
+// }
+
+ 
+   myToken()
 
 
 
